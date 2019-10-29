ae31d64bfe0b4abebcc5a62cd053f9edd020ffbb
Lucas Sampaio <lucasssouza29@hotmail.com>
2019-10-29 11:19:33 -0300

    Adicionado metodo de extensão para incluir as propriedades de navegação passada por parametro na query

---
+++ GestaoEmpresa.Repositorio/RepositorioExtension/DbSetExtensio.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestaoEmpresa.Repositorio.RepositorioExtension
+{
+    public static class DbSetExtensio
+    {
+        public static IQueryable<T> DynamicInclude<T>(this DbSet<T> dbSet, params string[] props) where T : class
+        {
+            var propriedades = typeof(T).GetProperties();
+            var query = dbSet.AsQueryable();
+            for (int i = 0; i < props.Length; i++)
+            {
+                if (propriedades.Any(x => x.Name == props[i]))
+                    query = query.Include(props[i]);
+            }
+            return query;
+        }
+    }
+}
