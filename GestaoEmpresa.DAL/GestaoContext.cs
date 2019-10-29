7572467f2d14717feb4b7c0197cbaf12e4a7bbb2
Lucas Sampaio <lucasssouza29@hotmail.com>
2019-10-29 09:34:32 -0300

    Adicionado o contexto

---
+++ GestaoEmpresa.DAL/GestaoContext.cs
@@ -0,0 +1,21 @@
+﻿using GestaoEmpresa.Dominio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestaoEmpresa.DAL
+{
+    class GestaoContext : DbContext
+    {
+        public GestaoContext() : base() { }
+        public GestaoContext(DbContextOptions<GestaoContext> options = null) : base(options)
+        {
+
+        }
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<JornadaTrabalho> Jornadas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
